87315a8c6082252a1d8f7aa1fe8a2f5483cbe69c
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-10-22 10:53:39 -0700

    Droplet module outputs

---
+++ modules/droplet/outputs.tf
@@ -0,0 +1,11 @@
+output "hostname" {
+    value = "${digitalocean_droplet.droplet.name}"
+}
+
+output "ip_v4_address_public" {
+    value = "${digitalocean_droplet.droplet.ipv4_address}"
+}
+
+output "ip_v4_address_private" {
+    value = "${digitalocean_droplet.droplet.ipv4_address_private}"
+}
