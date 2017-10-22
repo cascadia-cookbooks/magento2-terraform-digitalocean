a29eceb73085117f248ab0cd37abcd53170de104
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-10-22 10:53:08 -0700

    Droplet module

---
+++ modules/droplet/main.tf
@@ -0,0 +1,12 @@
+# DigitalOcean droplet
+resource "digitalocean_droplet" "droplet" {
+    count = "${var.droplet_count}"
+    image = "${var.droplet_image}"
+    name = "${var.droplet_name}.${count.index + 1}"
+    region = "${var.droplet_region}"
+    size = "${var.droplet_size}"
+    backups = "${var.droplet_backups}"
+    private_networking = "${var.droplet_private_networking}"
+    ssh_keys = "${var.droplet_ssh_keys}"
+    user_data = "${var.droplet_user_data}"
+}
