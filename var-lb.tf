3968e565114234f919e5a0f1f0a4f2e4fde1d510
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-10-22 10:07:28 -0700

    Health check variables

--- var-lb.tf
+++ var-lb.tf
@@ -19,3 +19,38 @@ variable "lb_sticky_sessions_cookie_ttl" {
     description = "Sticky session lifetime in seconds"
     default = 300
 }
+
+variable "lb_health_check_protocol" {
+    description = "Protocol for load balancer health check"
+    default = "http"
+}
+
+variable "lb_health_check_port" {
+    description = "Port for load balancer health check"
+    default = 80
+}
+
+variable "lb_health_check_path" {
+    description = "Path for load balancer health check"
+    default = "/"
+}
+
+variable "lb_health_check_interval" {
+    description = "Health check interval (seconds) for load balancer"
+    default = 10
+}
+
+variable "lb_health_check_timeout" {
+    description = "Health check timeout (seconds) for load balancer"
+    default = 5
+}
+
+variable "lb_health_check_unhealthy_threshold" {
+    description = "Number of health check failures before removal from pool"
+    default = 3
+}
+
+variable "lb_health_check_healthy_threshold" {
+    description = "Number of health check successes before inclusion in pool"
+    default = 3
+}
