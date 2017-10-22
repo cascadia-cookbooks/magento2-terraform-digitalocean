3a49e57ba04d6b427afaa95a84fd86971df70060
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-10-22 11:00:49 -0700

    Include environment in droplet name

--- droplets.tf
+++ droplets.tf
@@ -3,7 +3,7 @@ module "web" {
 
     droplet_count = "${var.web_count}"
     droplet_region = "${var.region}"
-    droplet_name = "${var.project_id}.${var.web_name}"
+    droplet_name = "${var.project_id}.${var.environment}.${var.web_name}"
     droplet_ssh_keys = "${var.web_ssh_keys}"
     droplet_user_data = "${var.web_user_data}"
 }
@@ -13,7 +13,7 @@ module "data" {
 
     droplet_count = "${var.data_count}"
     droplet_region = "${var.region}"
-    droplet_name = "${var.project_id}.${var.data_name}"
+    droplet_name = "${var.project_id}.${var.environment}.${var.data_name}"
     droplet_ssh_keys = "${var.data_ssh_keys}"
     droplet_user_data = "${var.data_user_data}"
 }
