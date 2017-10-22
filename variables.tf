f694f1513360b8d5fd9951e3c62e81fdfe2437b7
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-10-21 18:35:28 -0700

    Assign variable from LastPass data

---
+++ variables.tf
@@ -0,0 +1,12 @@
+# Pulls variables from LastPass
+data "external" "lpass" {
+    query = {
+        project = "${var.lastpass-project}"
+    }
+    program = ["bash", "${path.module}/scripts/lpass-data.sh"]
+}
+
+variable "lastpass-project" {
+    description = "Project name in LastPass"
+    default = "Magento 2 Template"
+}
