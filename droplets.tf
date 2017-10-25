2a92181809724a1ba7a2ddc935d264df63edcc3b
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-10-24 19:58:56 -0700

    Formatting

--- droplets.tf
+++ droplets.tf
@@ -1,19 +1,19 @@
 module "web" {
     source = "modules/droplet"
 
-    droplet_count = "${var.web_count}"
-    droplet_region = "${var.region}"
-    droplet_name = "${var.project_id}.${var.environment}.${var.web_name}"
-    droplet_ssh_keys = "${var.web_ssh_keys}"
+    droplet_count     = "${var.web_count}"
+    droplet_region    = "${var.region}"
+    droplet_name      = "${var.project_id}.${var.environment}.${var.web_name}"
+    droplet_ssh_keys  = "${var.web_ssh_keys}"
     droplet_user_data = "${var.web_user_data}"
 }
 
 module "data" {
     source = "modules/droplet"
 
-    droplet_count = "${var.data_count}"
-    droplet_region = "${var.region}"
-    droplet_name = "${var.project_id}.${var.environment}.${var.data_name}"
-    droplet_ssh_keys = "${var.data_ssh_keys}"
+    droplet_count     = "${var.data_count}"
+    droplet_region    = "${var.region}"
+    droplet_name      = "${var.project_id}.${var.environment}.${var.data_name}"
+    droplet_ssh_keys  = "${var.data_ssh_keys}"
     droplet_user_data = "${var.data_user_data}"
 }
